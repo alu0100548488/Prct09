3a46b63e963ffda44baf426bc2820735caecb35e
Daniel Mateos <DaniMateos@MacBook-Pro-de-Dani.local>
2013-11-22 02:26:56 +0000

    Creación matriz densa

--- lib/matrixfamily.rb
+++ lib/matrixfamily.rb
@@ -2,6 +2,10 @@ require "matrixfamily/version"
 
 require "./lib/matrixfamily/fraccion.rb"
 require "./lib/matrixfamily/gcd.rb"
+require "./lib/matrixfamily/version.rb"
+require "./lib/matrixfamily/matriz.rb"
+require "./lib/matrixfamily/matrizdensa.rb"
+require "./lib/matrixfamily/matrizdispersa.rb"
 
 module Matrixfamily
   # Your code goes here...
