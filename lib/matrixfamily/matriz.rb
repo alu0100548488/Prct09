9d16544cf5229ffb133d7064b34e608e50f443f8
Alejandro <alejandro@alejandro-VirtualBox.(none)>
2013-11-22 03:25:16 +0000

    Maximo y mínimo prueba

--- lib/matrixfamily/matriz.rb
+++ lib/matrixfamily/matriz.rb
@@ -16,6 +16,74 @@ class Matriz
     @dimension = col.size #Guardamos en una variable de instancia la dimension de la matriz.
   
   end
+  
+  def maximo
+   
+    if @matriz.is_a? Array then
+      
+      max = @matriz[0][0]
+      
+      for i in 0...@dimension
+        for j in 0...@dimension
+          if @matriz[i][j] > max
+            max = @matriz[i][j]
+          end
+        end
+      end
+      
+      max
+      
+    elsif @matrix.is_a? Hash then      
+      
+      max = @matrix[0][0]      
+        
+      for i in @matrix.keys do
+        for j in @matrix[i].keys do
+          if @matrix[i][j] > max
+            max = @matrix[i][j]
+	  end          
+	end
+      end
+      
+      max
+      
+    end
+    
+  end
+
+  def minimo    
+            
+    if @matriz.is_a? Array then
+      
+      min = @matriz[0][0]
+      
+      for i in 0...@dimension
+        for j in 0...@dimension
+          if @matriz[i][j] < min
+            min = @matriz[i][j]
+          end
+        end
+      end
+      
+      min
+      
+    elsif @matrix.is_a? Hash then
+    
+      min = @matrix[@matrix.keys.first][@matrix[@matrix.keys.first].keys.first]
+      
+      for i in @matrix.keys do      
+	for j in @matrix[i].keys do
+	  if @matrix[i][j] < min && @matrix[i][j] != 0 then
+            min = @matrix[i][j]
+          end
+        end
+      end
+      
+      min
+      
+    end
+  
+  end
 
   def [] (*ij) #Recibe un numero de argumentos...
   
